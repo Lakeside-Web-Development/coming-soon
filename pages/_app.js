df2767ce315ceea2c30c7a37895c26742f34d00d
Devin Conway <devinthomas1208@gmail.com>
2023-06-26 23:26:39 -0400

    :wrench: fixed build error & moved around some code

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
-import "@/styles/globals.css";
+import { analytics } from '@/scripts/analytics';
+import '@/styles/globals.css';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 library.add(fab, fas);
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  useEffect(() => {
+    analytics(window, document, 'script', 'dataLayer', process.env.GTAG);
+  });
+  return (
+    <>
+      <Head>
+        <title>Lakeside Web Development</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <title>Lakeside Web Development</title>
         <meta name='title' content='Lakeside Web Development' />
         <meta
           name='description'
